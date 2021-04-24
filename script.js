cb0972942fee947dd2b20a55aa5f77696747f48c
Emmanuel Jaygbay <emmanueljaygbay@gmail.com>
2021-04-24 14:59:23 +0000

    blink added to current element for playing.

--- script.js
+++ script.js
@@ -7,9 +7,12 @@ let init_space = 5;
 
 const drawItems = () => {
     let margin_bottom = init_space;
+    document.querySelector("#tower1").innerHTML = "";
+    document.querySelector("#tower2").innerHTML = "";
+    document.querySelector("#tower3").innerHTML = "";
     if (tower1.length > 0) {
         tower1.reverse().forEach(ele => {
-            let html = `<div class="item" id="item${ele}" style="margin-bottom:${margin_bottom}px">${ele}</div>`;
+            let html = `<div class="item" id="item-${ele}" style="margin-bottom:${margin_bottom}px" onclick="getClickItemID(event)">${ele}</div>`;
             document.querySelector("#tower1").insertAdjacentHTML("beforeend", html);
             margin_bottom += space;
         })
@@ -18,7 +21,7 @@ const drawItems = () => {
 
     if (tower2.length > 0) {
         tower2.reverse().forEach(ele => {
-            let html = `<div class="item" id="item${ele}" style="margin-bottom:${margin_bottom}px">${ele}</div>`;
+            let html = `<div class="item" id="item-${ele}" style="margin-bottom:${margin_bottom}px" onclick="getClickItemID(event)">${ele}</div>`;
             document.querySelector("#tower2").insertAdjacentHTML("beforeend", html);
             margin_bottom += space;
         })
@@ -27,12 +30,51 @@ const drawItems = () => {
 
     if (tower3.length > 0) {
         tower3.reverse().forEach(ele => {
-            let html = `<div class="item" id="item${ele}" style="margin-bottom:${margin_bottom}px">${ele}</div>`;
+            let html = `<div class="item" id="item-${ele}" style="margin-bottom:${margin_bottom}px" onclick="getClickItemID(event)">${ele}</div>`;
             document.querySelector("#tower3").insertAdjacentHTML("beforeend", html);
             margin_bottom += space;
         })
         margin_bottom = init_space;
     }
+
+    tower1.reverse();
+    tower2.reverse();
+    tower3.reverse();
 }
 
-drawItems()
+drawItems();
+
+const getClickItemID = (e) => {
+    let id = e.target.id;
+    let parent_ele_id = e.target.parentElement.id;
+    let item = Number(id.split("-")[1]);
+
+    removeClass('is-playing');
+    addClass(id, 'is-playing');
+
+
+
+    if (parent_ele_id === 'tower1') {
+        // tower1.splice(tower1.indexOf(item), 1);
+        // tower2.push(item);
+        // drawItems()
+    }
+    // else if(parent_ele_id === 'tower2'){
+
+    // }else{
+
+    // }
+    // console.log(`${parent_ele_id}`)
+    // console.log("id=", position)
+}
+
+const addClass = (id, class_name) => {
+    document.querySelector(`#${id}`).classList.add(class_name);
+}
+
+const removeClass = (class_name) => {
+    let playing = document.querySelector(`.${class_name}`);
+    if (playing) {
+        playing.classList.remove(class_name);
+    }
+}
