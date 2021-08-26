49f05ebf1d64e2183c1c3197d4f0cdd8233b481a
Emmanuel Jaygbay <emmanueljaygbay@gmail.com>
2021-08-26 11:10:02 +0000

    Selection section added before game starts.

--- script.js
+++ script.js
@@ -1,12 +1,33 @@
-let tower1 = [8, 7, 6, 5, 4, 3, 2, 1];
+let tower1 = [];
 let tower2 = [];
 let tower3 = [];
 let space = 40;
 let init_space = 5;
 let num_of_moves = -1;
 
+document.querySelector("#start-btn").addEventListener('click', (e) => {
+    let num_selected = document.querySelector("#select-num").value;
 
-const drawItems = () => {
+    if (num_selected > 0) {
+        hideSection("selection-section");
+        showSection("game-board");
+
+        for (let i = num_selected; i > 0; i--) {
+            tower1.push(i);
+        }
+        drawItems(tower1);
+    }
+})
+
+const hideSection = (ele_id) => {
+    document.querySelector(`#${ele_id}`).style = "display: none";
+}
+
+const showSection = (ele_id) => {
+    document.querySelector(`#${ele_id}`).style = "display: block";
+}
+
+const drawItems = (items) => {
     let margin_bottom = init_space;
     num_of_moves += 1;
     document.querySelector("#tower1").innerHTML = "";
@@ -62,7 +83,6 @@ const drawItems = () => {
     console.log("Moves:", num_of_moves)
 }
 
-drawItems();
 
 const getClickItemID = (e) => {
     let id = e.target.id;
@@ -97,13 +117,13 @@ const selectToPlay = (e) => {
                         tower1.splice(tower1.indexOf(item), 1);
                         tower2.push(item);
                         removeClass('is-playing');
-                        drawItems();
+                        drawItems(tower1);
                     }
                 } else {
                     tower1.splice(tower1.indexOf(item), 1);
                     tower2.push(item);
                     removeClass('is-playing');
-                    drawItems();
+                    drawItems(tower1);
                 }
             } else if (area_to_play === 'tower3') {
                 if (tower3.length > 0) {
@@ -111,13 +131,13 @@ const selectToPlay = (e) => {
                         tower1.splice(tower1.indexOf(item), 1);
                         tower3.push(item);
                         removeClass('is-playing');
-                        drawItems();
+                        drawItems(tower1);
                     }
                 } else {
                     tower1.splice(tower1.indexOf(item), 1);
                     tower3.push(item);
                     removeClass('is-playing');
-                    drawItems();
+                    drawItems(tower1);
                 }
             }
         } else if (parent_ele_id === 'tower2') {
@@ -128,13 +148,13 @@ const selectToPlay = (e) => {
                         tower2.splice(tower2.indexOf(item), 1);
                         tower1.push(item);
                         removeClass('is-playing');
-                        drawItems();
+                        drawItems(tower1);
                     }
                 } else {
                     tower2.splice(tower2.indexOf(item), 1);
                     tower1.push(item);
                     removeClass('is-playing');
-                    drawItems();
+                    drawItems(tower1);
                 }
 
             } else if (area_to_play === 'tower3') {
@@ -143,13 +163,13 @@ const selectToPlay = (e) => {
                         tower2.splice(tower2.indexOf(item), 1);
                         tower3.push(item);
                         removeClass('is-playing');
-                        drawItems();
+                        drawItems(tower1);
                     }
                 } else {
                     tower2.splice(tower2.indexOf(item), 1);
                     tower3.push(item);
                     removeClass('is-playing');
-                    drawItems();
+                    drawItems(tower1);
                 }
             }
 
@@ -161,13 +181,13 @@ const selectToPlay = (e) => {
                         tower3.splice(tower3.indexOf(item), 1);
                         tower1.push(item);
                         removeClass('is-playing');
-                        drawItems();
+                        drawItems(tower1);
                     }
                 } else {
                     tower3.splice(tower3.indexOf(item), 1);
                     tower1.push(item);
                     removeClass('is-playing');
-                    drawItems();
+                    drawItems(tower1);
                 }
 
             } else if (area_to_play === 'tower2') {
@@ -176,13 +196,13 @@ const selectToPlay = (e) => {
                         tower3.splice(tower3.indexOf(item), 1);
                         tower2.push(item);
                         removeClass('is-playing');
-                        drawItems();
+                        drawItems(tower1);
                     }
                 } else {
                     tower3.splice(tower3.indexOf(item), 1);
                     tower2.push(item);
                     removeClass('is-playing');
-                    drawItems();
+                    drawItems(tower1);
                 }
 
 
@@ -194,3 +214,39 @@ const selectToPlay = (e) => {
         }
     }
 }
+
+let target_num = 4;
+let target_pole = 3;
+
+let t1_last_num = t2_last_num = t3_last_num = 0;
+
+const solver = () => {
+    console.log(getLastNumber(tower1))
+    t1_last_num = getLastNumber(tower1);
+    t2_last_num = getLastNumber(tower2);
+    t3_last_num = getLastNumber(tower3);
+
+    if (target_num % 2 === 0) {
+        if (target_pole === 2) {
+            initMove(getLastNumber(tower1), tower1, tower3);
+            initMove(getLastNumber(tower1), tower1, tower2);
+        } else {
+            initMove(getLastNumber(tower1), tower1, tower2);
+            initMove(getLastNumber(tower1), tower1, tower3);
+        }
+    } else {
+
+    }
+}
+
+const getLastNumber = (tower) => {
+    return (tower[tower1.length - 1]);
+}
+
+const initMove = (num, from, to) => {
+    from.splice(from.indexOf(num));
+    to.push(num);
+    drawItems(tower1);
+}
+
+// solver();
